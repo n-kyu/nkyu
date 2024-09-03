f642ed17cba466226238fe52876df27503b853fb
n-kyu <112785244+n-kyu@users.noreply.github.com>
2024-09-03 14:26:35 -0300

    feat: add projects page at /projects

---
+++ src/pages/projects/index.tsx
@@ -0,0 +1,9 @@
+import Inner from "../../components/Layout/Inner"
+
+export default function Projects() {
+    return (
+        <Inner>
+            <h1>Projects Page</h1>
+        </Inner>
+    )
+}
