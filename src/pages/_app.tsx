a8ee16f19dfaa5014783278b1ff826a34b5c08fe
n-kyu <112785244+n-kyu@users.noreply.github.com>
2024-09-03 14:28:21 -0300

    feat: add menu and integrate AnimatePresence in _app.tsx
    - Added imports for Link and AnimatePresence
    - Implemented a simple menu
    - Configured AnimatePresence with `mode="wait"`

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,19 @@
 import "@/styles/globals.css";
+import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
+import Link from "next/link";
 
-export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />;
+export default function App({ Component, pageProps, router }: AppProps) {
+  return (
+    <div className="main">
+      <div className="header flex gap-6 text-center text-4xl mb-9">
+        <Link href='/'>Home</Link>
+        <Link href='/projects'>Projects</Link>
+        <Link href='/about'>About</Link>
+      </div>
+      <AnimatePresence mode="wait">
+        <Component key={router.route}  {...pageProps} />;
+      </AnimatePresence>
+    </div>
+  )
 }
