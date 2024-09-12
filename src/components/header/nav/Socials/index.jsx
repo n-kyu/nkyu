bc15f076225f5c81fdd11b1ea7c4c76f9eb82566
n-kyu <112785244+n-kyu@users.noreply.github.com>
2024-09-12 15:30:59 -0300

    feat: add Socials component for social links

---
+++ src/components/header/nav/Socials/index.jsx
@@ -0,0 +1,11 @@
+import styles from './style.module.css';
+
+export default function Index() {
+  return (
+    <div className={styles.menuSocials}>
+        <a>LinkedIn</a>
+        <a>Github</a>
+        <a>Resume</a>
+    </div>
+  )
+}
