caa8d8fd41036b1fa671d2595d8b80e1eef4c96d
n-kyu <112785244+n-kyu@users.noreply.github.com>
2024-09-12 15:22:24 -0300

    feat: import and use new components Header and StickyCursor
    - Imported Header and StickyCursor components
    - Integrated StickyCursor and Header into _app.tsx
    - Removed old header component

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,16 +1,16 @@
 import "@/styles/globals.css";
 import { AnimatePresence } from "framer-motion";
 import type { AppProps } from "next/app";
-import Link from "next/link";
+import Header from '../components/header'
+import StickyCursor from '../components/StickyCursor'
+
+
 
 export default function App({ Component, pageProps, router }: AppProps) {
   return (
     <div className="main">
-      <div className="header flex gap-6 text-center text-4xl mb-9">
-            <Link href="/">Home</Link>
-            <Link href="/projects">Projects</Link>
-            <Link href="/about">About</Link>
-          </div>
+      <StickyCursor stickyElement={undefined}/>
+      <Header />
       <AnimatePresence mode="wait">
         <Component key={router.route}  {...pageProps} />;
       </AnimatePresence>
