050ff58d0350f2dead7ca1b18230226e251304cf
n-kyu <112785244+n-kyu@users.noreply.github.com>
2024-09-12 15:27:46 -0300

    feat: integrate HERO, ABOUTME, and PROJECTS components into Home page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,9 @@
 import { Inter } from "next/font/google";
 import Head from "next/head";
 import Stairs from "../components/Layout/Stairs"
+import Hero from '../components/Hero/'
+import AboutMe from '../components/AboutMeSection'
+import Projects from '../components/ProjectsSection'
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -13,12 +16,10 @@ export default function Home() {
         <meta name="viewport" content="width=device-width,initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-
+      <Hero />
       <Stairs>
-        <h1>Home Page</h1>
-        <div>
-          <p>lorem ipsum</p>
-        </div>
+        <AboutMe />
+        <Projects />
       </Stairs>
     </>
   );
